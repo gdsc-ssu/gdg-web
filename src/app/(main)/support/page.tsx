741e459551b6ffa4a2733d1a59ce9422b1736996
DevDan09 <devyun09@gmail.com>
2025-05-03 15:42:22 +0900

    ✨ SupportPage에서 SupporterPortraitGroup으로 변경 및 설명 수정

--- src/app/(main)/support/page.tsx
+++ src/app/(main)/support/page.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next';
-import SupporterPortrait from '@/app/components/SupporterPortrait';
-import SupporterCard from '@/app/components/SupporterCard';
-import { SupporterInfo } from './supporter.d';
-import { supporters } from './supporters';
+import SupporterPortraitGroup from '@/app/components/support/SupporterPortraitGroup';
+import SupporterCard from '@/app/components/support/SupporterCard';
+import { SupporterInfo } from '@/types/supporter';
+import { supporters } from './supporters';  
 
 export const metadata: Metadata = {
   title: 'Support | GDSC Soongsil',
-  description: 'GDSC Soongsil을 후원하고 지원하는 방법을 안내합니다.',
+  description: 'GDSC Soongsil의 발전을 위해 도움 주신 분들입니다.',
 };
 
 const SupporterCardFactory = ({ supporters }: { supporters: Array<SupporterInfo> }) => {
@@ -32,10 +32,10 @@ export default function SupportPage() {
         ">
           Support
         </h1>
-        <h1 className="text-center text-primary-blue text-xl font-medium mt-20">
+        <h1 className="text-center text-primary-blue text-xl font-medium m-20">
         GDGoC Soongsil의 발전을 위해 도움 주신 분들입니다.
         </h1>
-        <SupporterPortrait />
+        <SupporterPortraitGroup images={supporters.map((supporter) => supporter.profile)} names={supporters.map((supporter) => supporter.name)} />
         <SupporterCardFactory supporters={supporters} />
         <div className="h-[200px]"> </div>
       </section>
