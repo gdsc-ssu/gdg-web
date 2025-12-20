5697a20f4ac9068cbe49adc0a90f1d6fdd2c96f5
DevDAN09 <devyun09@gmail.com>
2025-12-20 21:06:47 +0900

    feat: 푸터 컴포넌트와 소셜 미디어 아이콘을 추가

---
+++ src/app/components/common/Footer.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const SOCIAL_LINKS = [
+    { name: 'Github', href: 'https://github.com/gdgoc-ssu', icon: '/5th/footer/Github.svg' },
+    { name: 'Instagram', href: 'https://www.instagram.com/gdgoc_ssu', icon: '/5th/footer/Instagram.svg' },
+    { name: 'LinkedIn', href: 'https://www.linkedin.com/company/gdgoc-ssu', icon: '/5th/footer/LinkedIn.svg' },
+    { name: 'YouTube', href: 'https://www.youtube.com/@gdgoc_ssu', icon: '/5th/footer/YouTube.svg' },
+];
+
+const Footer = () => {
+    return (
+        <footer className="w-full flex justify-center items-center py-10 px-[48px] bg-white text-neutral-grey text-[12px] font-medium border-t border-neutral-light-grey">
+            <div className="w-full max-w-[1280px] flex flex-col md:flex-row justify-between items-center gap-6 md:gap-0">
+
+                {/* Left: Organization Name */}
+                <div className="font-semibold text-neutral-black">
+                    GDGoC Soongsil
+                </div>
+
+                {/* Center: Copyright */}
+                <div className="text-center text-neutral-grey">
+                    Copyright © 2025 Google Developer Groups on Campus Soongsil All Rights Reserved.
+                </div>
+
+                {/* Right: Social Icons */}
+                <div className="flex gap-4">
+                    {SOCIAL_LINKS.map((social) => (
+                        <Link
+                            key={social.name}
+                            href={social.href}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative w-6 h-6 opacity-60 hover:opacity-100 transition-opacity"
+                        >
+                            <Image
+                                src={social.icon}
+                                alt={social.name}
+                                fill
+                                className="object-contain"
+                            />
+                        </Link>
+                    ))}
+                </div>
+
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
