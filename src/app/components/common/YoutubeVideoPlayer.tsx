524fb5e9e884c29efc8764b1cb3aa6639f570183
DevDAN09 <devyun09@gmail.com>
2025-12-20 21:53:47 +0900

    chore : deprecated 속성 삭제

--- src/app/components/common/YoutubeVideoPlayer.tsx
+++ src/app/components/common/YoutubeVideoPlayer.tsx
@@ -6,7 +6,6 @@ const YoutubeVideoPlayer = ({ videoId }: { videoId: string }) => {
                 height="100%"
                 src={`https://www.youtube.com/embed/${videoId}`}
                 title="YouTube video player"
-                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
             />
